3b7f419ea20bc7470b757e56c956259b04f9162b
lvivduncan <lvivduncan@gmail.com>
2020-05-04 22:13:20 +0300

    new parallax

--- js/levus-parallax.js
+++ js/levus-parallax.js
@@ -2,11 +2,8 @@
   // обгортка
   const wrapper = document.querySelector('.wrapper');
 
-  // відступ згори до паралаксу
-  const marginTop = wrapper.getBoundingClientRect().top;
-
-  // відступ до нижнього краю
-  const marginBottom = wrapper.getBoundingClientRect().top + wrapper.getBoundingClientRect().height;
+  // відступ згори від батьківського елемента (у нашому випадку -- body) до паралаксу
+  const offsetTop = wrapper.offsetTop;
 
   // елементи 
   const articles = document.querySelectorAll('.wrapper article');
@@ -14,12 +11,26 @@
   // вішаємо подію "скрол"
   window.addEventListener('scroll', () => {
 
-    // час вмикання анімації
-    if (window.pageYOffset > marginTop && window.pageXOffset < marginBottom) {
-      articles.forEach(article => {
+    // розмір вікна
+    const hightWindow = document.documentElement.clientHeight;
+
+    // відстань прокрутки
+    const scroll = window.pageYOffset;
+
+    articles.forEach(article => {
+      
+      // час вмикання анімації (коли розмір видимого вікна + прокрутка більша за відступ до блока)
+      if (hightWindow + scroll > offsetTop) {
+
         // при прокрутці фон отримує зсув
-        article.style.backgroundPositionY = `-${(window.pageYOffset - marginTop) / 2}px`;
-      });
-    }
+        article.style.backgroundPositionY = `-${(hightWindow + scroll - offsetTop) / 3}px`;
+      } else {
+
+        // видаляємо стилі
+        article.removeAttribute('style');
+      }
+
+    });
+    
   });
 }
