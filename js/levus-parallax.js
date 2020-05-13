2303742a1ca6a967775afc05c9b737a006b494eb
lvivduncan <lvivduncan@gmail.com>
2020-05-13 17:17:48 +0300

    added a check

--- js/levus-parallax.js
+++ js/levus-parallax.js
@@ -1,36 +1,38 @@
 {
   // обгортка
-  const wrapper = document.querySelector('.wrapper');
+  const parallax = document.querySelector('.levus-parallax');
 
   // відступ згори від батьківського елемента (у нашому випадку -- body) до паралаксу
-  const offsetTop = wrapper.offsetTop;
+  const offsetTop = parallax.offsetTop;
 
   // елементи 
-  const articles = document.querySelectorAll('.wrapper article');
+  const articles = document.querySelectorAll('.levus-parallax article');
 
-  // вішаємо подію "скрол"
-  window.addEventListener('scroll', () => {
+  if (parallax != 'undefined' && parallax != null) {
+    // вішаємо подію "скрол"
+    window.addEventListener('scroll', () => {
 
-    // розмір вікна
-    const hightWindow = document.documentElement.clientHeight;
+      // розмір вікна
+      const hightWindow = document.documentElement.clientHeight;
 
-    // відстань прокрутки
-    const scroll = window.pageYOffset;
+      // відстань прокрутки
+      const scroll = window.pageYOffset;
 
-    articles.forEach(article => {
-      
-      // час вмикання анімації (коли розмір видимого вікна + прокрутка більша за відступ до блока)
-      if (hightWindow + scroll > offsetTop) {
+      articles.forEach(article => {
 
-        // при прокрутці фон отримує зсув
-        article.style.backgroundPositionY = `-${(hightWindow + scroll - offsetTop) / 3}px`;
-      } else {
+        // час вмикання анімації (коли розмір видимого вікна + прокрутка більша за відступ до блока)
+        if (hightWindow + scroll > offsetTop) {
 
-        // видаляємо стилі
-        article.removeAttribute('style');
-      }
+          // при прокрутці фон отримує зсув
+          article.style.backgroundPositionY = `-${(hightWindow + scroll - offsetTop) / 3}px`;
+        } else {
+
+          // видаляємо стилі
+          article.removeAttribute('style');
+        }
+
+      });
 
     });
-    
-  });
+  }
 }
